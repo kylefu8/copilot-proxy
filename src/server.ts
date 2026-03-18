614e530edc2c529533d6aae92307c28c7bf5ec1a
kylefu8 <kylefu8@gmail.com>
2026-03-18 20:34:31 +0800

    feat: add conversation recording middleware (based on upstream v0.4.1)
    Cherry-picked from conv-middleware branch, zero conflicts.
    Non-invasive Hono middleware: only 2 lines in server.ts + 1 new file.

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,11 @@ import { modelRoutes } from './routes/models/route'
 import { responsesRoutes } from './routes/responses/route'
 import { tokenRoute } from './routes/token/route'
 import { usageRoute } from './routes/usage/route'
+import { conversationMiddleware } from './lib/conversation-middleware'
 
 export const server = new Hono()
 
+server.use(conversationMiddleware)
 server.use(logger())
 server.use(cors())
 
