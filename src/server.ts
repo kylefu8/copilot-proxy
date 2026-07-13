0c813dd572113b24c6d839a97691571e4f790f10
kylefu8 <kylefu8@gmail.com>
2026-07-13 20:48:04 +0800

    chore: align conversation middleware import

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { logger } from 'hono/logger'
 
 import { NATIVE_SERVICE_INSTANCE_HEADER } from '~/daemon/native-service'
 import { withApprovalRequestContext } from '~/lib/approval'
+import { conversationMiddleware } from '~/lib/conversation-middleware'
 import { isRequestHostAllowed, isRequestOriginAllowed, resolveCorsOrigin } from '~/lib/security'
 import { state } from '~/lib/state'
 
@@ -15,8 +16,6 @@ import { modelRoutes } from './routes/models/route'
 import { responsesRoutes } from './routes/responses/route'
 import { tokenRoute } from './routes/token/route'
 import { usageRoute } from './routes/usage/route'
-import { conversationMiddleware } from './lib/conversation-middleware'
-
 export const server = new Hono()
 
 server.use(conversationMiddleware)
